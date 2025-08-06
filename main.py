e94a2a97bc5ecfdee01897cca73e807c535b016a
abiheda <abisolahd@gmail.com>
2025-08-06 15:42:06 +0100

    more stuff

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import requests
+base_url = "https://www.themealdb.com/api/json/v1/1"
+
+def get_meal_info(meal):
+    url = f"{base_url}/search.php?s={meal}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return -1
+    
+
+get_meal_info ('Arrabiata')
+get_meal_info('Samuel')
